2a828a7b6ec9346b4e6761bd7b844cc73152a9ea
renaco <renanromero@gmail.com>
2022-11-10 17:21:01 -0500

    test: add captcha for environment

--- src/components/newsletter/index.tsx
+++ src/components/newsletter/index.tsx
@@ -3,10 +3,12 @@ import ReCAPTCHA from 'react-google-recaptcha';
 import axios from 'axios';
 
 export const NewsLetter = () => {
+
+  const localEnv = process.env;
   const urlMail = 'https://newsletterdev.eluniversal.com.mx/v1/newsletter/register-new-user';
   const logoNewsLetter = 'https://www.viveusa.mx/sites/all/modules/widget_newsletter/css/logo-viveusa.svg';
   const messages = {
-    default: 'Recibe en tu correo las noticias más destacadas para viajar, trabajar y vivir en EU', 
+    default: 'Recibe en tu correo las noticias más destacadas para viajar, trabajar y vivir en EU',
     success: 'Gracias por registrarte, te enviamos un correo para confirmar tu dirección.'
   }
   const emailRegEx = '/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i';
@@ -25,7 +27,7 @@ export const NewsLetter = () => {
   }
 
   const handleGetSubscriptor = (event: string) => {
-    setSubscriptor({...subscriptor, email: event})
+    setSubscriptor({ ...subscriptor, email: event })
     console.log('subscriptor', subscriptor)
   }
 
@@ -34,63 +36,62 @@ export const NewsLetter = () => {
     console.log('handleSubscription', event);
 
     axios.post(urlMail, subscriptor)
-    .then((response) => {
-      console.log('response.data', response.data);
-      setMessage(messages.success);
-    })
+      .then((response) => {
+        console.log('response.data', response.data);
+        setMessage(messages.success);
+      })
   }
+
+  console.log('localEnv', localEnv)
 
   return (
     <Fragment>
-      <div className="contenedor-gral-widget">
-        <div className="m-[10px]">
-          <img src={logoNewsLetter} alt="viveusa logo" className="block w-5 logo-top" />
-          <div className="texto-cabeza">Newsletter</div>
+      <div className="contenedor-gral-widget m-[10px] p-5">
+        <div className="my-5 flex justify-between">
+          <img src={logoNewsLetter} alt="viveusa logo" className="block w-[60px] md:w-[120px]" />
+          <h3 className="texto-cabeza">Newsletter</h3>
         </div>
-        <p className="texto-principal news-email-send-ok"
+        <p
+          className="texto-principal news-email-send-ok"
           data-gtm-vis-first-on-screen-37354430_60="184455"
           data-gtm-vis-recent-on-screen-37354430_60="1069624"
           data-gtm-vis-total-visible-time-37354430_60="100"
           data-gtm-vis-has-fired-37354430_60="1"
         >{message}</p>
-        <div style={{ clear: 'both' }}></div>
         <div
-          className="widgetnw bottom-[10px]">
+          className="bottom-[10px]">
           <form
-            // onSubmit=
-            className="formgrid"
+            className="flex flex-col"
             id="register-form-newsletter-11">
-            <div className="input-box texto">
-              <input
-                type="hidden"
-                value="nwsl311"
-              />
+            <div className="">
               <input
                 className="inputwg"
                 type="email"
                 placeholder="TU E-MAIL"
                 required
-                onChange={(e) => {handleGetSubscriptor(e.target.value)}}
+                onChange={(e) => { handleGetSubscriptor(e.target.value) }}
                 pattern={emailRegEx}
               />
               <button
-                className="btnWidgetNews"
+                className=""
                 disabled={buttonStatus}
                 onClick={handleSubscription}
               >Suscribirme</button>
-              <span
-                className="disclaimer"
-              >Al registrarme acepto los <a rel="noreferrer" href="https://www.eluniversal.com.mx/politicas-de-privacidad"
-                target="_blank"
-                className="linktyc"
-              >términos y condiciones</a>
-            </span>
             </div>
+            <span
+              className="text-[11px] color-[#d3eefd] mt-2"
+            >Al registrarme acepto los <a rel="noreferrer" href="https://www.eluniversal.com.mx/politicas-de-privacidad"
+              target="_blank"
+              className="underline"
+            >términos y condiciones</a>
+            </span>
+            <section className="flex center mt-4">
+              <ReCAPTCHA
+                sitekey="6LeKyu4iAAAAAKbGfVcG7mMtNNAy6rivGN3D-iSX"
+                onChange={handleStatusButton}
+              ></ReCAPTCHA>
+            </section>
           </form>
-          <ReCAPTCHA
-            sitekey="6LeKyu4iAAAAAKbGfVcG7mMtNNAy6rivGN3D-iSX"
-            onChange={handleStatusButton}
-          ></ReCAPTCHA>
         </div>
       </div>
     </Fragment>
