391f12ef5170c550901fec2840c13c2733c02304
renaco <renanromero@gmail.com>
2022-12-16 17:58:09 -0500

    chore: redefine constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,125 +1,140 @@
 // colors
-const colorWatermelonade = "rgb(232, 74, 80)";
-const colorDungeonKeeper = "rgb(237, 49, 59)";
-const colorOttomanRed = "rgb(238, 33, 45)";
-const colorHeatSignature = "rgb(221, 0, 17)";
-const colorErrigalWhite = "rgb(240, 242, 247)";
-const colorLostAtSea = "rgb(135, 152, 173)";
-const colorGoodKarma = "rgb(42, 51, 120)";
-const colorLavaStone = "rgb(58, 64, 91)";
-const colorLavaStone2 = "rgb(57, 63, 91)";
-const colorCloudless = "rgb(211, 238, 253)";
-const colorWingCommander = "rgb(0, 100, 181)";
-const colorGrandBleu = "rgb(0, 83, 131)";
-const colorCascara = "rgb(241, 67, 60)";
-const colorWhite = "rgba(255, 255, 255, 0.75)";
-const colorWhite2 = "rgb(255, 255, 255)";
-const colorVintagePorcelain = "rgb(238, 238, 238)";
-const colorConcrete = "rgb(208, 208, 208)";
-const colorPigeon = "rgb(174, 174, 174)";
-const colorQuicksilver = "rgb(165, 165, 165)";
-const colorWalrus = "rgb(155, 155, 155)";
-const colorExcalibur = "rgb(102, 102, 102)";
-const colorBlack = "rgb(0, 0, 0)";
+export const colors = {
+  Watermelonade: "rgb(232, 74, 80)",
+  DungeonKeeper: "rgb(237, 49, 59)",
+  OttomanRed: "rgb(238, 33, 45)",
+  HeatSignature: "rgb(221, 0, 17)",
+  ErrigalWhite: "rgb(240, 242, 247)",
+  LostAtSea: "rgb(135, 152, 173)",
+  GoodKarma: "rgb(42, 51, 120)",
+  LavaStone: "rgb(58, 64, 91)",
+  LavaStone2: "rgb(57, 63, 91)",
+  Cloudless: "rgb(211, 238, 253)",
+  WingCommander: "rgb(0, 100, 181)",
+  GrandBleu: "rgb(0, 83, 131)",
+  Cascara: "rgb(241, 67, 60)",
+  White: "rgba(255, 255, 255, ,75)",
+  White2: "rgb(255, 255, 255)",
+  VintagePorcelain: "rgb(238, 238, 238)",
+  Concrete: "rgb(208, 208, 208)",
+  Pigeon: "rgb(174, 174, 174)",
+  Quicksilver: "rgb(165, 165, 165)",
+  Walrus: "rgb(155, 155, 155)",
+  Excalibur: "rgb(102, 102, 102)",
+  Black: "rgb(0, 0, 0)",
+}
 
 // typography 
-const font1 = `normal normal 700 61px/72px "work sans", sans-serif`;
-const font2 = `normal normal 700 40px/44px "work sans", sans-serif`;
-const font3 = `normal normal 700 40px/44px "work sans", sans-serif`;
-const font4 = `normal normal 300 35px/42px "work sans", sans-serif`;
-const font5 = `normal normal 500 30px/25px "work sans", sans-serif`;
-const font6 = `normal normal 300 30px/normal "work sans", sans-serif`;
-const font7 = `normal normal 700 28px/31px "work sans", sans-serif`;
-const font8 = `normal normal 900 27px/27px "font awesome 5 free"`;
-const font9 = `normal normal 400 24px/28px rubik, Helvetica, Arial, serif`;
-const font10 = `normal normal 600 20px/24px "work sans", sans-serif`;
-const font11 = `normal normal 600 20px/24px "work sans", sans-serif`;
-const font12 = `normal normal 300 20px/24px "work sans", sans-serif`;
-const font13 = `normal normal 600 20px/24px "work sans", sans-serif`;
-const font14 = `normal normal 400 18px/18px "font awesome 5 brands"`;
-const font15 = `normal normal 400 18px/19px "work sans", sans-serif`;
-const font16 = `normal normal 600 18px/21px "work sans", sans-serif`;
-const font17 = `normal normal 400 18px/18px "font awesome 5 brands"`;
-const font18 = `normal normal 400 18px/21px "work sans", sans-serif`;
-const font19 = `normal normal 600 18px/21px "work sans", sans-serif`;
-const font20 = `normal normal 400 17px/normal "work sans", sans-serif`;
-const font21 = `normal normal 900 16px/16px "font awesome 5 free"`;
-const font22 = `normal normal 400 16px/19px "work sans", sans-serif`;
-const font23 = `normal normal 700 16px/19px "work sans", sans-serif`;
-const font24 = `normal normal 400 16px/normal "work sans", sans-serif`;
-const font25 = `normal normal 400 16px/normal "Times New Roman"`;
-const font26 = `normal normal 400 16px/normal "work sans", sans-serif`;
-const font27 = `normal normal 400 16px/21px "work sans", sans-serif`;
-const font28 = `normal normal 900 16px/16px "font awesome 5 free"`;
-const font29 = `normal normal 400 16px/21px "work sans", sans-serif`;
-const font30 = `normal normal 400 16px/normal "Times New Roman"`;
-const font31 = `normal normal 700 16px/normal "work sans", sans-serif`;
-const font32 = `normal normal 400 16px/normal "work sans", sans-serif`;
-const font33 = `normal normal 400 16px/normal "work sans", sans-serif`;
-const font34 = `normal normal 900 16px/16px "font awesome 5 free"`;
-const font35 = `normal normal 400 16px/20px "work sans", sans-serif`;
-const font36 = `normal normal 300 16px/20px "work sans", sans-serif`;
-const font37 = `normal normal 400 16px/normal "work sans", sans-serif`;
-const font38 = `normal normal 500 15px/21px "work sans", sans-serif`;
-const font39 = `normal normal 400 14px/14px "font awesome 5 free"`;
-const font40 = `normal normal 700 14px/18px "work sans", sans-serif`;
-const font41 = `normal normal 700 14px/18px "work sans", sans-serif`;
-const font42 = `normal normal 300 14px/24px "work sans", sans-serif`;
-const font43 = `normal normal 700 14px/24px "work sans", sans-serif`;
-const font44 = `normal normal 300 14px/18px "work sans", sans-serif`;
-const font45 = `normal normal 500 14px/normal "work sans", sans-serif`;
-const font46 = `normal normal 500 14px/16px "work sans", sans-serif`;
-const font47 = `normal normal 400 13.3333px/normal Arial`;
-const font48 = `normal normal 400 13.3333px/normal Arial`;
-const font49 = `normal normal 600 13.3333px/normal Arial`;
-const font50 = `normal normal 400 13px/15px "work sans", sans-serif`;
-const font51 = `normal normal 900 12px/normal "work sans", sans-serif`;
-const font52 = `normal normal 400 11px/normal "work sans", sans-serif`;
-const font53 = `normal normal 400 11px/normal "work sans", sans-serif`;
+export const typography =
+{
+  font1: `normal normal 700 61px/72px "work sans", sans-serif`,
+  font2: `normal normal 700 40px/44px "work sans", sans-serif`,
+  font3: `normal normal 700 40px/44px "work sans", sans-serif`,
+  font4: `normal normal 300 35px/42px "work sans", sans-serif`,
+  font5: `normal normal 500 30px/25px "work sans", sans-serif`,
+  font6: `normal normal 300 30px/normal "work sans", sans-serif`,
+  font7: `normal normal 700 28px/31px "work sans", sans-serif`,
+  font8: `normal normal 900 27px/27px "font awesome 5 free"`,
+  font9: `normal normal 400 24px/28px rubik, Helvetica, Arial, serif`,
+  font10: `normal normal 600 20px/24px "work sans", sans-serif`,
+  font11: `normal normal 600 20px/24px "work sans", sans-serif`,
+  font12: `normal normal 300 20px/24px "work sans", sans-serif`,
+  font13: `normal normal 600 20px/24px "work sans", sans-serif`,
+  font14: `normal normal 400 18px/18px "font awesome 5 brands"`,
+  font15: `normal normal 400 18px/19px "work sans", sans-serif`,
+  font16: `normal normal 600 18px/21px "work sans", sans-serif`,
+  font17: `normal normal 400 18px/18px "font awesome 5 brands"`,
+  font18: `normal normal 400 18px/21px "work sans", sans-serif`,
+  font19: `normal normal 600 18px/21px "work sans", sans-serif`,
+  font20: `normal normal 400 17px/normal "work sans", sans-serif`,
+  font21: `normal normal 900 16px/16px "font awesome 5 free"`,
+  font22: `normal normal 400 16px/19px "work sans", sans-serif`,
+  font23: `normal normal 700 16px/19px "work sans", sans-serif`,
+  font24: `normal normal 400 16px/normal "work sans", sans-serif`,
+  font25: `normal normal 400 16px/normal "Times New Roman"`,
+  font26: `normal normal 400 16px/normal "work sans", sans-serif`,
+  font27: `normal normal 400 16px/21px "work sans", sans-serif`,
+  font28: `normal normal 900 16px/16px "font awesome 5 free"`,
+  font29: `normal normal 400 16px/21px "work sans", sans-serif`,
+  font30: `normal normal 400 16px/normal "Times New Roman"`,
+  font31: `normal normal 700 16px/normal "work sans", sans-serif`,
+  font32: `normal normal 400 16px/normal "work sans", sans-serif`,
+  font33: `normal normal 400 16px/normal "work sans", sans-serif`,
+  font34: `normal normal 900 16px/16px "font awesome 5 free"`,
+  font35: `normal normal 400 16px/20px "work sans", sans-serif`,
+  font36: `normal normal 300 16px/20px "work sans", sans-serif`,
+  font37: `normal normal 400 16px/normal "work sans", sans-serif`,
+  font38: `normal normal 500 15px/21px "work sans", sans-serif`,
+  font39: `normal normal 400 14px/14px "font awesome 5 free"`,
+  font40: `normal normal 700 14px/18px "work sans", sans-serif`,
+  font41: `normal normal 700 14px/18px "work sans", sans-serif`,
+  font42: `normal normal 300 14px/24px "work sans", sans-serif`,
+  font43: `normal normal 700 14px/24px "work sans", sans-serif`,
+  font44: `normal normal 300 14px/18px "work sans", sans-serif`,
+  font45: `normal normal 500 14px/normal "work sans", sans-serif`,
+  font46: `normal normal 500 14px/16px "work sans", sans-serif`,
+  font47: `normal normal 400 13.3333px/normal Arial`,
+  font48: `normal normal 400 13.3333px/normal Arial`,
+  font49: `normal normal 600 13.3333px/normal Arial`,
+  font50: `normal normal 400 13px/15px "work sans", sans-serif`,
+  font51: `normal normal 900 12px/normal "work sans", sans-serif`,
+  font52: `normal normal 400 11px/normal "work sans", sans-serif`,
+  font53: `normal normal 400 11px/normal "work sans", sans-serif`,
+}
 
 // vertical spacing
-const vspace1 = "1px";
-const vspace2 = "3px";
-const vspace3 = "4px";
-const vspace4 = "5px";
-const vspace5 = "7px";
-const vspace6 = "10px";
-const vspace7 = "12px";
-const vspace8 = "15px";
-const vspace9 = "1em";
-const vspace10 = "17px";
-const vspace11 = "18px";
-const vspace12 = "20px";
-const vspace13 = "25px";
-const vspace14 = "26px";
+export const verticalSpacing = {
+  vspace1: "1px",
+  vspace2: "3px",
+  vspace3: "4px",
+  vspace4: "5px",
+  vspace5: "7px",
+  vspace6: "10px",
+  vspace7: "12px",
+  vspace8: "15px",
+  vspace9: "1em",
+  vspace10: "17px",
+  vspace11: "18px",
+  vspace12: "20px",
+  vspace13: "25px",
+  vspace14: "26px",
+}
 
 // horizontal spacing
-const hspace1 = "1px";
-const hspace2 = "3px";
-const hspace3 = "5px";
-const hspace4 = "-5px";
-const hspace5 = "-7px";
-const hspace6 = "8px";
-const hspace7 = "10px";
-const hspace8 = "12px";
-const hspace9 = "15px";
-const hspace10 = "17px";
-const hspace11 = "20px";
-const hspace12 = "29px";
-const hspace13 = "470px";
-const hspace14 = "551px";
-const hspace15 = "-682px";
+export const horizontalSpacing = {
+  hspace1: "1px",
+  hspace2: "3px",
+  hspace3: "5px",
+  hspace4: "-5px",
+  hspace5: "-7px",
+  hspace6: "8px",
+  hspace7: "10px",
+  hspace8: "12px",
+  hspace9: "15px",
+  hspace10: "17px",
+  hspace11: "20px",
+  hspace12: "29px",
+  hspace13: "470px",
+  hspace14: "551px",
+  hspace15: "-682px",
+}
 
 // border radii
-const borderRadius1 = "50%";
-const borderRadius2 = "5px 0px 0px 5px";
-const borderRadius3 = "0px 5px 5px 0px";
-const borderRadius4 = "5px";
-const borderRadius5 = "4px";
+export const borderRadius = {
+  borderRadius1: "50%",
+  borderRadius2: "5px 0px 0px 5px",
+  borderRadius3: "0px 5px 5px 0px",
+  borderRadius4: "5px",
+  borderRadius5: "4px",
+}
 
 // shadows
-const shadow1 = "rgba(221, 221, 221, 0.5) 0px 2px 4px 0px";
+export const shadows = {
+  shadow1: "rgba(221, 221, 221, 0.5) 0px 2px 4px 0px",
+}
 
 // motions
-const transition1 = "all 0.5s ease 0s";
-const transition2 = "all 0.4s ease-out 0s";
+export const motions = {
+  transition1: "all 0.5s ease 0s",
+  transition2: "all 0.4s ease-out 0s",
+}
