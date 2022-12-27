1ed6321b798340f30d355ca71ce5f544d3c12ab5
renaco <renanromero@gmail.com>
2022-12-27 12:03:59 -0500

    wip: hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import { NewsLetter } from './components/newsletter'
+import { Hero } from './components/hero'
 
 function App() {
   return (
     <div className="App">
+      <Hero />
       <NewsLetter />
     </div>
   );
---
+++ src/components/hero/index.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+export const Hero = () => {
+  return (
+    <picture>
+      <div
+        className="relative p-0 mx-auto mt-0 mb-2 w-full font-sans border-sky-800 text-sky-800 relative"
+        data-gtm-vis-recent-on-screen-37354429_179={197354}
+        data-gtm-vis-first-on-screen-37354429_179={197355}
+        data-gtm-vis-total-visible-time-37354429_179={100}
+        data-gtm-vis-has-fired-37354429_179={1}
+        style={{ outline: 0, gridColumnEnd: "span 12", maxWidth: 1080 }}
+      >
+        <img
+          className="inline p-0 m-0 w-full text-sky-800 absolute top-0 left-0 z-0"
+          src="https://www.eluniversal.com.mx/sites/default/files/styles/f01-1080x400_corte_1_u2020/public/2021/06/08/elecciones_2021_0.jpg?itok=nHTag0TC"
+          alt="Estos fueron los resultados electorales en México 2021"
+          title=""
+          loading="lazy"
+          style={{
+            display: "inline",
+            height: 400,
+            outline: 0,
+            filter: "brightness(0.8)",
+            animationName: "filtro2",
+            animationDuration: "4s",
+            animationDelay: "2s"
+          }}
+        />
+        <h3
+          className="p-0 m-0 text-5xl font-bold text-center text-sky-800 absolute top-[50%] left-[50%] z-1"
+          style={{
+            // lineHeight: 60,
+            outline: 0,
+            // textShadow: "rgba(0, 0, 0, 0.75) 1px 1px"
+          }}
+        >
+          <a
+            href="nacion/politica/estos-fueron-los-resultados-electorales-en-mexico-2021"
+            target="_self"
+            className="p-0 m-0 text-5xl font-bold text-center text-white hover:cursor-pointer hover:text-sky-800"
+            style={{
+              outline: 0,
+              textDecoration: "none",
+              // textShadow: "rgba(0, 0, 0, 0.75) 1px 1px 0px",
+              // lineHeight: 60
+            }}
+          >
+            Estos fueron los resultados electorales en México 2021
+          </a>
+          <p
+            className="hidden my-2 mx-auto text-lg font-light leading-5 text-center text-white"
+            // style={{ maxWidth: "60%", textShadow: "rgba(0, 0, 0, 0.75) 1px 1px" }}
+          >
+            Los resultados de las elecciones 2021 sorprendieron a más de uno
+          </p>
+          <button
+            className="table py-2 px-12 my-4 mx-auto font-sans font-semibold tracking-wider leading-3 text-center text-black uppercase bg-white border-2 border-white border-solid cursor-default bg-opacity-[0]"
+            style={{
+              outline: 0,
+              borderRadius: 100,
+              transition: "all 0.4s ease-out 0s",
+              fontSize: 11,
+              textShadow: "rgba(0, 0, 0, 0.75) 1px 1px"
+            }}
+          >
+            <a
+              href="nacion/politica/estos-fueron-los-resultados-electorales-en-mexico-2021"
+              target="_self"
+              rel="bookmark"
+              className="p-0 m-0 font-sans font-semibold tracking-wider leading-3 text-center text-white uppercase hover:cursor-pointer hover:text-sky-800"
+              style={{
+                outline: 0,
+                textDecoration: "none",
+                fontSize: 11,
+                textShadow: "rgba(0, 0, 0, 0.75) 1px 1px 0px"
+              }}
+            >
+              VER
+            </a>
+          </button>
+        </h3>
+      </div>
+    </picture>
+  )
+}
