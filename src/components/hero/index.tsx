a0cd64ff3545f8b13994b48e91b8a62385c816c3
renaco <renanromero@gmail.com>
2022-12-28 10:03:40 -0500

    styles: define text and sizes

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -12,61 +12,41 @@ export const Hero = () => {
     >
       <img
         className="inline p-0 m-0 w-full text-sky-800 hero_image"
-        src="https://www.eluniversal.com.mx/sites/default/files/styles/f01-1080x400_corte_1_u2020/public/2021/06/08/elecciones_2021_0.jpg?itok=nHTag0TC"
+        src="https://www.eluniversal.com.mx/sites/default/files/styles/f01-1080x400_corte_1_u2020/public/2021/11/23/imagen_boletines2.jpg?itok=P9pNnnfd"
         alt="Estos fueron los resultados electorales en México 2021"
         title=""
         loading="lazy"
       />
       <div
-        className="absolute top-1/2 left-1/2 p-0 m-0 w-full border-sky-800 text-sky-800 font-['Work_Sans']"
-        style={{ outline: 0, maxWidth: 900, transform: "translate(-50%, -50%)" }}
+        className="absolute top-1/2 left-1/2 p-0 m-0 w-full border-sky-800 text-sky-800 font-['Work_Sans'] hero_wrapper"
       >
         <h3
-          className="p-0 m-0 text-5xl font-bold text-center text-sky-800"
-          style={{
-            outline: 0,
-            textShadow: "rgba(0, 0, 0, 0.75) 1px 1px"
-          }}
+          className="p-0 m-0 font-bold text-center text-sky-800 is-shadow"
         >
           <a
             href="nacion/politica/estos-fueron-los-resultados-electorales-en-mexico-2021"
             target="_self"
-            className="p-0 m-0 text-3xl lg:text-5xl font-bold text-center text-white hover:cursor-pointer hover:text-sky-800"
-            style={{
-              outline: 0,
-              textDecoration: "none",
-              textShadow: "rgba(0, 0, 0, 0.75) 1px 1px 0px",
-            }}
+            className="p-0 m-0 text-3xl lg:text-[56px] lg:leading-[60px] font-bold text-center text-white hover:cursor-pointer hover:text-sky-800 no-underline is-shadow"
           >
-            Estos fueron los resultados electorales en México 2021
+            Más cerca de ti: conoce y suscríbete a nuestros newsletters
           </a>
           <p
-            className="hidden md:visible my-2 mx-auto text-lg font-light leading-5 text-center text-white"
-            style={{ maxWidth: "60%", textShadow: "rgba(0, 0, 0, 0.75) 1px 1px" }}
+            className="hidden md:block my-2 mx-auto text-base lg:text-lg font-light leading-5 text-center text-white is-shadow"
           >
-            Los resultados de las elecciones 2021 sorprendieron a más de uno
+            En EL UNIVERSAL tenemos una oferta de boletines para que estés bien informado, conócelos, suscríbete y pasa la voz
           </p>
           <button
-            className="table py-2 px-12 my-4 mx-auto font-sans font-semibold tracking-wider leading-3 text-center text-black uppercase bg-white border-2 border-white border-solid cursor-default bg-opacity-[0]"
+            className="table py-2 px-12 my-4 mx-auto font-sans font-semibold tracking-wider leading-3 text-center text-black uppercase bg-white border-2 border-white border-solid cursor-default bg-opacity-[0] is-shadow"
             style={{
-              outline: 0,
               borderRadius: 100,
               transition: "all 0.4s ease-out 0s",
-              fontSize: '11px',
-              textShadow: "rgba(0, 0, 0, 0.75) 1px 1px"
             }}
           >
             <a
               href="nacion/politica/estos-fueron-los-resultados-electorales-en-mexico-2021"
               target="_self"
               rel="bookmark"
-              className="p-0 m-0 font-semibold tracking-wider leading-3 text-center text-white uppercase hover:cursor-pointer hover:text-sky-800 font-['Work_Sans']"
-              style={{
-                outline: 0,
-                textDecoration: "none",
-                fontSize: '11px',
-                textShadow: "rgba(0, 0, 0, 0.75) 1px 1px 0px"
-              }}
+              className="p-0 m-0 font-semibold tracking-wider leading-3 text-center text-white uppercase hover:cursor-pointer hover:text-sky-800 font-['Work_Sans'] no-underline text-[11px] is-shadow"
             >
               VER
             </a>
